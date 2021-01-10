64f564dc42febc9c0905fb9b4e2b895df5cf7406
imanimarthe12 <imanimarthe12@gmail.com>
2021-01-10 12:18:16 +0200

    Errors fixed

--- js/app.js
+++ js/app.js
@@ -3,5 +3,5 @@ const indexButton = document.querySelector('#indexReg');
 indexButton.addEventListener('click', gotoRegister);
 
 function gotoRegister(){
-    window.location.href='../html/register.html'
+    window.location.href='../html/login.html'
 }
